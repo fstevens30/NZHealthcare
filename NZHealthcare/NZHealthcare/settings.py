cf7bec3b4d350b07e71a37e43698d0f99f8a4bac
Flynn Stevens <flynnstevens30@gmail.com>
2023-11-16 14:08:46 +1300

    Fixed styling errors

--- NZHealthcare/NZHealthcare/settings.py
+++ NZHealthcare/NZHealthcare/settings.py
@@ -116,9 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
