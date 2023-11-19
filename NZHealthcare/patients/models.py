aa9844ccc63079873cffc01329c2d76ac287bab1
Flynn Stevens <flynnstevens30@gmail.com>
2023-11-20 10:19:56 +1300

    Updated model fields for effieciency

--- NZHealthcare/patients/models.py
+++ NZHealthcare/patients/models.py
@@ -4,17 +4,17 @@ from django.db import models
 
 
 class Patient(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=15)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=80)
+    phone_number = models.CharField(max_length=12)
     date_of_birth = models.DateField()
 
 
 class Referral(models.Model):
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
     referral_date = models.DateField()
-    referrer_name = models.CharField(max_length=255)
-    referral_reason = models.TextField(max_length=255)
+    referrer_name = models.CharField(max_length=150)
+    referral_reason = models.TextField(max_length=150)
     note = models.TextField()
     document = models.FileField(
         upload_to='referral_documents/', null=True, blank=True)
