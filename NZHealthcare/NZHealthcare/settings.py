7d51b88a5e40d3261a68a888103f392452f6f721
Flynn Stevens <flynnstevens30@gmail.com>
2023-11-20 10:49:04 +1300

    Updates to help with login/logout

--- NZHealthcare/NZHealthcare/settings.py
+++ NZHealthcare/NZHealthcare/settings.py
@@ -128,3 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+# Login and Logout redirect
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
